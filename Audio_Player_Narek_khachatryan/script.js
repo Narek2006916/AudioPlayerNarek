5f7536cebc5c160151f547310d11fca6c2c372dd
Narek Khachatryan <narekkhachatryan@MacBook-Pro-Narek.local>
2024-05-13 12:54:21 +0400

    homework

--- Audio_Player_Narek_khachatryan/script.js
+++ Audio_Player_Narek_khachatryan/script.js
@@ -1,45 +1,47 @@
-let data  = {
-    title : [
+let data = {
+    title: [
         "James Brown – It's A Man's, Man's, Man's World",
         "Pink Floyd - Shine on You Crazy Diamond",
-        "Gary Moore – Still Got The Blues"
+        "Gary Moore – Still Got The Blues",
     ],
-    song : [
+    song: [
         "music/James Brown – It's A Man's, Man's, Man's World.mp3",
         "music/Pink Floyd - Shine on You Crazy Diamond (Parts 1-5).mp3",
-        "Gary Moore – Still Got The Blues.mp3"
+        "music/Gary Moore – Still Got The Blues.mp3",
     ],
     poster: [
-        "https://images.pexels.com/photos/1105666/pexels-photo-1105666.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
         "https://i1.sndcdn.com/artworks-000044442011-50k6ec-t500x500.jpg",
-        "https://i.scdn.co/image/ab67616d0000b273b586fc87d8bde3ba953f233a"
+        "https://i1.sndcdn.com/artworks-000044442011-50k6ec-t500x500.jpg",
+        "https://i.scdn.co/image/ab67616d0000b273b586fc87d8bde3ba953f233a",
     ],
 }
 
-
 let song = new Audio();
+let currentSong = 0;
 
-window.onload = function (){
-    playSong()
+window.onload = function() {
+    playSong();
 }
-let currentSong = 0
-function playSong(){
+
+function playSong() {
     song.src = data.song[currentSong];
-    let songTitle = document.getElementById("songTitle")
-    songTitle.textContent = data.title[currentSong]
-    let img = document.getElementsByClassName("row1")
-    img[0].style.backgroundImage = "url("+ data,poster[currentSong] +")"
-    let main = document.getElementsByClassName("main");
-    main[0].style.backgroundImage = "url("+ data,poster[currentSong] +")"
+    let songTitle = document.getElementById("songTitle");
+    songTitle.textContent = data.title[currentSong];
+    let img = document.getElementsByClassName("row1");
+    img[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")";
+    // let main = document.getElementsByClassName("main");
+    // main[0].style.backgroundImage = "url(" + data.poster[currentSong] + ")";
 }
 
-function playOrPause(){
-        let play = document.getElementById("play")
-        if(song.paused){
-            song.play()
-            play.src = "images/pause.png"
-        }else{
-            song.pause()
-            play.src = "images/play-button-arrowhead.png"
-        }
+function playOrPause() {
+    let play = document.getElementById("play");
+    if (song.paused) {
+        song.play();
+        play.classList.remove("fa-play");
+        play.classList.add("fa-pause");
+    } else {
+        song.pause();
+        play.classList.remove("fa-pause");
+        play.classList.add("fa-play");
+    }
 }
