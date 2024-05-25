a25443ca6e0a8c0463da05cbd1d80f52a0446038
sargsyan53 <tigran.sargsyan12@tumo.org>
2024-05-25 10:57:45 +0400

    full-project-no

--- Audio_Player_Narek_khachatryan/script.js
+++ Audio_Player_Narek_khachatryan/script.js
@@ -1,109 +1,133 @@
 let data = {
     title: [
-        "Back To Black",
+        // "Back To Black",
         "It's A Man's, Man's, Man's World",
         "Shine on You Crazy Diamond",
         "Still Got The Blues",
     ],
     singer: [
-        "Amy Winehouse",
+        // "Amy Winehouse",
         "James Brown",
         "Pink Floyd",
         "Gary Moore",
     ],
     song: [
-        "music/Amy_Winehouse-Back_To_Black.mp3",
+        // "music/Amy_Winehouse-Back_To_Black.mp3",
         "music/James Brown – It's A Man's, Man's, Man's World.mp3",
         "music/Pink Floyd - Shine on You Crazy Diamond (Parts 1-5).mp3",
         "music/Gary Moore – Still Got The Blues.mp3",
     ],
     poster: [
-        "images/amy-winehouse-back-2-high.jpg",
+        // "images/amy-winehouse-back-2-high.jpg",
         "images/James_Brown_the_man_world.jpg",
         "https://i1.sndcdn.com/artworks-000044442011-50k6ec-t500x500.jpg",
         "https://i.scdn.co/image/ab67616d0000b273b586fc87d8bde3ba953f233a",
     ],
     shadow: [
-        "0 0 50px 10px rgb(57 121 249 / 50%), 0 0 100px 20px rgba(165, 192, 255, 0.7)",
+        // "0 0 50px 10px rgb(57 121 249 / 50%), 0 0 100px 20px rgba(165, 192, 255, 0.7)",
         "0 0 50px 10px rgba(255, 255, 255, 0.5), 0 0 100px 20px rgba(255, 255, 255, 0.3)",
         "",
     ],
     lyrics: [
+        // [
+        //     { time: 0, text: "..." },
+        //     { time: 16, text: "He left no time to regret" },
+        //     { time: 22, text: "Kept his lips wet" },
+        //     { time: 25, text: "With his same old safe bet" },
+        //     { time: 31, text: "Me and my head high" },
+        //     { time: 37, text: "And my tears dry" },
+        //     { time: 40, text: "Get on without my guy" },
+        //     { time: 46, text: "You went back to what you knew" },
+        //     { time: 53, text: "So far removed" },
+        //     { time: 56, text: "From all that we went through" },
+        //     { time: 62, text: "And I tread a troubled track" },
+        //     { time: 68, text: "My odds are stacked" },
+        //     { time: 72, text: "I'll go back to black" },
+        //     { time: 78, text: "We only said goodbye with words" },
+        //     { time: 82, text: "I died a hundred times" },
+        //     { time: 86, text: "You go back to her" },
+        //     { time: 89, text: "And I go back to" },
+        //     { time: 93, text: "I go back to us" },
+        //     { time: 99, text: "I love you much" },
+        //     { time: 103, text: "It's not enough" },
+        //     { time: 105, text: "You love blow and I love puff" },
+        //     { time: 112, text: "And life is like a pipe" },
+        //     { time: 118, text: "And I'm a tiny penny rolling up the walls inside" },
+        //     { time: 129, text: "We only said goodbye with words" },
+        //     { time: 133, text: "I died a hundred times" },
+        //     { time: 137, text: "You go back to her" },
+        //     { time: 140, text: "And I go back to" },
+        //     { time: 143, text: "We only said goodbye with words" },
+        //     { time: 148, text: "I died a hundred times" },
+        //     { time: 152, text: "You go back to her" },
+        //     { time: 155, text: "And I go back to" },
+        //     { time: 158, text: "..." },
+        //     { time: 164, text: "Black, black, black, black" },
+        //     { time: 181, text: "Black, black, black" },
+        //     { time: 194, text: "I go back to" },
+        //     { time: 198, text: "I go back to" },
+        //     { time: 202, text: "We only said goodbye with words" },
+        //     { time: 206, text: "I died a hundred times" },
+        //     { time: 210, text: "You go back to her" },
+        //     { time: 213, text: "And I go back to" },
+        //     { time: 218, text: "We only said goodbye with words" },
+        //     { time: 222, text: "I died a hundred times" },
+        //     { time: 225, text: "You go back to her" },
+        //     { time: 228, text: "And I go back to" },
+        //     { time: 233, text: "Black" },
+        //     { time: 235, text: "..." },
+        // ],
         [
-            { time: 0, text: "..." },
-            { time: 16, text: "He left no time to regret" },
-            { time: 22, text: "Kept his lips wet" },
-            { time: 25, text: "With his same old safe bet" },
-            { time: 31, text: "Me and my head high" },
-            { time: 37, text: "And my tears dry" },
-            { time: 40, text: "Get on without my guy" },
-            { time: 46, text: "You went back to what you knew" },
-            { time: 53, text: "So far removed" },
-            { time: 56, text: "From all that we went through" },
-            { time: 62, text: "And I tread a troubled track" },
-            { time: 68, text: "My odds are stacked" },
-            { time: 72, text: "I'll go back to black" },
-            { time: 78, text: "We only said goodbye with words" },
-            { time: 82, text: "I died a hundred times" },
-            { time: 86, text: "You go back to her" },
-            { time: 89, text: "And I go back to" },
-            { time: 93, text: "I go back to us" },
-            { time: 99, text: "I love you much" },
-            { time: 103, text: "It's not enough" },
-            { time: 105, text: "You love blow and I love puff" },
-            { time: 112, text: "And life is like a pipe" },
-            { time: 118, text: "And I'm a tiny penny rolling up the walls inside" },
-            { time: 129, text: "We only said goodbye with words" },
-            { time: 133, text: "I died a hundred times" },
-            { time: 137, text: "You go back to her" },
-            { time: 140, text: "And I go back to" },
-            { time: 143, text: "We only said goodbye with words" },
-            { time: 148, text: "I died a hundred times" },
-            { time: 152, text: "You go back to her" },
-            { time: 155, text: "And I go back to" },
-            { time: 158, text: "..." },
-            { time: 164, text: "Black, black, black, black" },
-            { time: 181, text: "Black, black, black" },
-            { time: 194, text: "I go back to" },
-            { time: 198, text: "I go back to" },
-            { time: 202, text: "We only said goodbye with words" },
-            { time: 206, text: "I died a hundred times" },
-            { time: 210, text: "You go back to her" },
-            { time: 213, text: "And I go back to" },
-            { time: 218, text: "We only said goodbye with words" },
-            { time: 222, text: "I died a hundred times" },
-            { time: 225, text: "You go back to her" },
-            { time: 228, text: "And I go back to" },
-            { time: 233, text: "Black" },
-            { time: 235, text: "..." },
+            { "time": 0, "text": "..." },
+            { "time": 11, "text": "This is a man's world" },
+            { "time": 18, "text": "This is a man's world" },
+            { "time": 25, "text": "But it wouldn't be nothing" },
+            { "time": 30, "text": "Nothing without a woman or a girl" },
+            { "time": 38, "text": "You see, man made the cars" },
+            { "time": 42, "text": "To take us over the road" },
+            { "time": 46, "text": "Man made the train" },
+            { "time": 50, "text": "To carry the heavy load" },
+            { "time": 55, "text": "Man made the electric light" },
+            { "time": 60, "text": "To take us out of the dark" },
+            { "time": 65, "text": "Man made the boat for the water" },
+            { "time": 70, "text": "Like Noah made the ark" },
+            { "time": 75, "text": "This is a man's, man's, man's world" },
+            { "time": 82, "text": "But it wouldn't be nothing" },
+            { "time": 88, "text": "Nothing without a woman or a girl" },
+            { "time": 96, "text": "Man thinks about a little baby girls and a baby boys" },
+            { "time": 105, "text": "Man makes them happy" },
+            { "time": 110, "text": "'Cause man makes them toys" },
+            { "time": 115, "text": "And after man's made everything" },
+            { "time": 120, "text": "Everything he can" },
+            { "time": 125, "text": "You know that man makes money" },
+            { "time": 130, "text": "To buy from other man" },
+            { "time": 135, "text": "This is a man's world" },
+            { "time": 140, "text": "But it wouldn't be nothing" },
+            { "time": 145, "text": "Nothing without a woman or a girl" },
+            { "time": 153, "text": "He's lost in the wilderness" },
+            { "time": 158, "text": "He's lost in bitterness" },
+            { "time": 163, "text": "He's lost, lost and" },
+            { "time": 167, "text": "..." }
         ],
         [
-            { time: 0, text: "This is a man's world" },
-            { time: 5, text: "This is a man's world" },
-            { time: 10, text: "But it wouldn't be nothing, nothing" },
-            // Add more lines with timestamps
-        ],
-        [
-            { time: 0, text: "Remember when you were young, you shone like the sun" },
-            { time: 5, text: "Shine on you crazy diamond" },
-            // Add more lines with timestamps
+        { time: 0, text: "Remember when you were young, you shone like the sun" },
+        { time: 5, text: "Shine on you crazy diamond" },
         ],
         [
             { time: 0, text: "Used to be so easy to give my heart away" },
             { time: 5, text: "But I found out the hard way" },
             { time: 10, text: "There's a price you have to pay" },
-            // Add more lines with timestamps
         ]
     ]
 };
 let song = new Audio();
-song.volume = 0.5; 
+song.volume = 0.5;
 let currentSong = 0;
 let repeatMode = false;
 let shuffleMode = false;
 let shuffledIndices = [];
 
-window.onload = function() {
+window.onload = function () {
     playSong();
 };
 
@@ -147,7 +171,7 @@ function highlightLyric() {
     }
 }
 
-song.addEventListener("timeupdate", function() {
+song.addEventListener("timeupdate", function () {
     updateProgressBar();
     convertTime(song.currentTime);
     highlightLyric();
@@ -182,7 +206,7 @@ function playOrPause() {
     }
 }
 
-song.addEventListener("timeupdate", function() {
+song.addEventListener("timeupdate", function () {
     updateProgressBar();
     convertTime(song.currentTime);
     if (song.ended) {
@@ -202,14 +226,14 @@ function updateProgressBar() {
     seekBar.style.background = `linear-gradient(to right, #FFF ${position}%, #808080 ${position}%)`;
 }
 
-document.getElementById("seek-bar").addEventListener("input", function() {
+document.getElementById("seek-bar").addEventListener("input", function () {
     let seekBar = document.getElementById("seek-bar");
     let seekPosition = (seekBar.value / 100) * song.duration;
     song.currentTime = seekPosition;
     updateProgressBar();
 });
 
-document.getElementById("seek-bar").addEventListener("change", function() {
+document.getElementById("seek-bar").addEventListener("change", function () {
     let seekBar = document.getElementById("seek-bar");
     let seekPosition = (seekBar.value / 100) * song.duration;
     song.currentTime = seekPosition;
@@ -226,7 +250,7 @@ function convertTime(seconds) {
 }
 
 function updateTotalTime() {
-    song.onloadedmetadata = function() {
+    song.onloadedmetadata = function () {
         let totalTime = document.getElementById("totalTime");
         let min = Math.floor(song.duration / 60);
         let sec = Math.floor(song.duration % 60);
@@ -265,9 +289,9 @@ function toggleRepeat() {
     repeatMode = !repeatMode;
     let repeatButton = document.querySelector('.fa-repeat');
     if (repeatMode) {
-        repeatButton.style.color = '#EAF0FF'; 
+        repeatButton.style.color = '#EAF0FF';
     } else {
-        repeatButton.style.color = 'rgba(137, 150, 184, 0.6)'; 
+        repeatButton.style.color = 'rgba(137, 150, 184, 0.6)';
     }
 }
 
@@ -275,10 +299,10 @@ function toggleShuffle() {
     shuffleMode = !shuffleMode;
     let shuffleButton = document.querySelector('.fa-shuffle');
     if (shuffleMode) {
-        shuffleButton.style.color = '#EAF0FF'; 
+        shuffleButton.style.color = '#EAF0FF';
         shuffledIndices = generateShuffledIndices();
     } else {
-        shuffleButton.style.color = 'rgba(137, 150, 184, 0.6)'; 
+        shuffleButton.style.color = 'rgba(137, 150, 184, 0.6)';
         shuffledIndices = [];
     }
 }
@@ -319,19 +343,19 @@ function toggleVolumeSlider() {
     }
 }
 
-document.getElementById("volume-slider").addEventListener("input", function() {
+document.getElementById("volume-slider").addEventListener("input", function () {
     song.volume = this.value / 100;
     this.style.background = `linear-gradient(to right, #fff ${this.value}%, #808080 ${this.value}%)`;
     const volume = document.getElementById("volume")
-    if(this.value == 0){
+    if (this.value == 0) {
         volume.classList.remove("fa-volume-low")
         volume.classList.add("fa-volume-xmark")
     }
-    else if(this.value == 100){
+    else if (this.value == 100) {
         volume.classList.remove("fa-volume-low")
         volume.classList.add("fa-volume-high")
     }
-    else if(this.value > 0 || this.value <= 99){
+    else if (this.value > 0 || this.value <= 99) {
         volume.classList.remove("fa-volume-xmark")
         volume.classList.remove("fa-volume-high")
         volume.classList.add("fa-volume-low")
